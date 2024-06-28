a1b3546e397714d1cbd386a22679a52cdd0ad79e
Eduardo Guevara <mobilemechaniceddie@gmail.com>
2024-06-27 17:06:38 -0700

    Finished The Calculator Visuals

---
+++ calc.js
@@ -0,0 +1,14 @@
+let buttonArea = document.querySelector('.buttonArea');
+
+const buttonValues = ['1', '4', '7', 'Back', '2', '5', '8', '0', '3', '6', '9', 'Clear', '()', '%', 'X', '+/-', '+', '-', '/', '='];
+
+function createButtons(){
+for(i = 0; i < 20; i++){
+    let button = document.createElement('button');
+    button.classList.add(`button`, `${buttonValues[i]}`);
+    button.textContent = buttonValues[i];
+    buttonArea.appendChild(button);
+}
+}
+
+createButtons();
